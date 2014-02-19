aaa78b2d8e7fdf0897d0cf5b1a013338a8853842
dolltih <xusitorax@gmail.com>
2014-02-18 23:09:45 -0500

    assignment3-P2-all

--- lab3/additiveSynthesis.py
+++ lab3/additiveSynthesis.py
@@ -8,7 +8,7 @@ from math import sin, pi
 numChannels = 1                      # mono
 sampleWidth = 2                      # in bytes, a 16-bit short
 sampleRate = 44100
-#MAX_AMP = 2**(8*sampleWidth - 1) - 1 #maximum amplitude is 2**15 - 1  = 32767 
+#MAX_AMP = 2**(8*sampleWidth - 1) - 1 #maximum amplitude is 2**15 - 1  = 32767
 
 # Take an array of shorts and write it out to a mono wave file
 
@@ -17,15 +17,18 @@ def writewav(fname, data, params):
         f.setparams(params)
         f.writeframes(data.tostring())
     print(fname + " written.")
-    
+
 
 def main():
 
     # parameters for this file
 
-    timFileName = raw_input("Enter the name of the timbre .tim.txt file: ")
-    envFileName = raw_input("Enter the name of the envelope .env.txt file: ")
-    #outfileName = raw_input("Enter the name of the output .wav file: ")
+    #timFileName = raw_input("Enter the name of the timbre .tim.txt file: ")
+    #envFileName = raw_input("Enter the name of the envelope .env.txt file: ")
+    outfileName = raw_input("Enter the name of the output .wav file: ")
+
+    timFileName = 'timbre.tim.txt'
+    envFileName = 'sinusoid.env.txt'
 
 
     data = array.array("h")
@@ -38,30 +41,32 @@ def main():
     freq =[]
     fact =[]
     while('' != item):
-        (freq[i],fact[i]) = item.rstrip().split('\t')
+        (freq0,fact0) = item.rstrip().split('\t')
+        freq.append(freq0)
+        fact.append(fact0)
         item = timFile.readline()
         i += 1
     timFile.close()
 
-    for j in range(0,i-1):
-        print(freq[j]+"\t"+fact[j]+"\n")
-
     envFile = open(envFileName,"r")
-    ampLeft = envFile.readline()
-    ampRight = envFile.readline()
-    windowSize = int(sampleRate * ampRight)
-    print(str(windowSize))
+    ampLeft = envFile.readline().split('\t')[1]
+    windowTime,ampRight = envFile.readline().split('\t')
+    windowSize = int(sampleRate * float(windowTime))
+
     i = 0
-    #while ('' != ampRight)
-    #   for j in range(0,windowSize):
-    #       
-    #       sample = MAX_AMP * sin( 2 * pi * 440.0 * i / sampleRate ) 
+    while ('' != ampRight):
+        for j in range(0,windowSize):
+            amp = (float(j) / windowSize) * int(ampRight) + (1 - (float(j) / windowSize)) * int(ampLeft)
+            sample = 0
+            for k in range(0,len(freq)):
+                sample += float(fact[k]) * amp * sin( 2 * pi * float(freq[k]) * (i+j) / sampleRate )
+            data.append( int( sample ) )
 
-    #    data.append( int( sample ) )
+        ampLeft = ampRight
+        ampRight = envFile.readline().split('\t')[1]
 
-
-    #params = [numChannels, sampleWidth, sampleRate , len(data), "NONE", None]
-    #writewav(outfileName, data, params)
+    params = [numChannels, sampleWidth, sampleRate , len(data), "NONE", None]
+    writewav(outfileName, data, params)
     envFile.close()
 
 main()
